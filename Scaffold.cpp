e646facb6c14320e0a607690610132efff1c2339
Adam Garcia <adamgarcia1994@ucla.edu>
2016-05-13 13:10:43 -0700

    Almost done with Scaffold implementation.  Just need undo feature.

--- Scaffold.cpp
+++ Scaffold.cpp
@@ -1,5 +1,6 @@
 // Scaffold.cpp
-
+#include <vector>
+#include <iostream>
 #include "provided.h"
 using namespace std;
 
@@ -14,39 +15,138 @@ public:
 	void display() const;
 	bool makeMove(int column, int color);
 	int undoMove();
+
+private:
+	vector<vector<int>> m_grid;//Add underlying datastructure to be a 2D vector.
+	int m_nRows;
+	int m_nCols;
+	vector<int> m_numInCol; //keeps tally of number of pieces in each column.
 };
 
 ScaffoldImpl::ScaffoldImpl(int nColumns, int nLevels)
 {
+	if (nColumns <= 0 || nLevels <= 0)
+	{
+		cerr << "Grid Size not positive.";
+		throw ("Grid Size not right.");
+	}
+	else
+	{
+		m_nRows = nLevels;
+		m_nCols = nColumns;
+		m_grid.resize(m_nRows); //Grid now has correct rows.
+		for (int i = 0;i<m_nRows;i++) //Go through each level.
+			m_grid[i].resize(m_nCols); //Each level has a column.
+		//access grid with m_grid[row][col]
+
+		cerr << "Scaffold Implemented" << endl;
+	}
+	for (int r = 0; r < m_nRows; r++)
+		for (int c = 0; c < m_nCols; c++)
+			m_grid[r][c] = VACANT;
+	//m_grid[n_rows - 1][n_cols - 1] = RED;
+
+	m_numInCol.resize(m_nCols);
+	//for (int i = 0; i < m_nCols; i++)
+	//{
+	//	m_numInCol[i] = 0;
+	//	cerr << m_numInCol[i] << " ";
+	//}
+		
 }
 
 int ScaffoldImpl::cols() const
 {
-	return 1;  //  This is not always correct; it's just here to compile
+	return m_nCols;  //  This is not always correct; it's just here to compile
 }
 
 int ScaffoldImpl::levels() const
 {
-	return 1;  //  This is not always correct; it's just here to compile
+	return m_nRows;  //  This is not always correct; it's just here to compile
 }
 
 int ScaffoldImpl::numberEmpty() const
 {
-	return 1;  //  This is not always correct; it's just here to compile
+	int counter = 0;
+	for (int r = 0; r < m_nRows; r++)
+		for (int c = 0; c < m_nCols; c++)
+			if (m_grid[r][c] == VACANT)
+				counter++;
+	return counter;  //  This is not always correct; it's just here to compile
 }
 
 int ScaffoldImpl::checkerAt(int column, int level) const
 {
-	return VACANT;  //  This is not always correct; it's just here to compile
+	int colPos = column - 1;
+	int levelPos = level - 1;
+	if (colPos <0 || colPos > m_nCols) //Check ranges
+		return VACANT;
+	else if (levelPos < 0 || levelPos > m_nRows)
+		return VACANT;
+	else
+		return m_grid[levelPos][colPos]; //returns value
+	return -97;  //Should never reach this.
 }
 
 void ScaffoldImpl::display() const
 {
+	int lvlMax = m_nRows - 1;
+	int colMax = m_nCols - 1;
+	//for (int i = m_nRows-1; i >= 0 ; i--)
+	//{
+	//	for (int j = 0; j < m_nCols; j++)
+	//		cout << m_grid[i][j]<< " ";
+	//	cout << endl;
+	//}
+
+	for (int i = lvlMax; i >= 0; i--)
+	{
+		cout << "|";
+		for (int j = 0; j <= colMax; j++)
+		{
+			switch (m_grid[i][j])
+			{
+			case RED:
+				cout << "R|";
+				break;
+			case BLACK:
+				cout << "B|";
+				break;
+			default:
+				cout << " |"; //change back to space
+				break;
+			}
+		}
+		cout << endl;
+	}
+	for (int i = 0; i <= colMax; i++)
+		cout << "+-";
+	cout << "+";
 }
 
 bool ScaffoldImpl::makeMove(int column, int color)
 {
-	return false;  //  This is not always correct; it's just here to compile
+	int colPos = column - 1;
+	if (colPos < 0 || column>m_nCols) //Column out of range
+	{
+		cerr << "Column selection out of range" << endl;
+		return false;
+	}
+	
+	int nextAvailLevel = m_numInCol[colPos];
+	if (nextAvailLevel >= m_nRows)
+	{
+		cerr << "Column: " << column << "is Fully Filled!" << endl;
+		return false;
+	}
+	if (color == RED || color == BLACK)
+	{
+		m_grid[nextAvailLevel][colPos] = color;
+		m_numInCol[colPos]++;
+		return true;
+	}
+	else
+		return false;  //  This is not always correct; it's just here to compile
 }
 
 int ScaffoldImpl::undoMove()
--- main.cpp
+++ main.cpp
@@ -7,14 +7,24 @@ using namespace std;
 
 int main()
 {
-	string name;
-	cout << "What's your name?  ";
-	getline(cin, name);
-	HumanPlayer hp(name);
-	SmartPlayer sp("Computer");
-	Game g1(4, 3, 3, &hp, &sp);
-	g1.play();
-	cout << endl << "This time I'll go first." << endl;
-	Game g2(4, 3, 3, &sp, &hp);
-	g2.play();
+	Scaffold s(3, 2);
+	s.makeMove(2, RED);
+	s.makeMove(1, BLACK);
+	s.makeMove(1, RED);
+	s.display();
+	//  |R| | |
+	//  |B|R| |
+	//  +-+-+-+
+	cin.ignore(1000, '\n');
+
+	//string name;
+	//cout << "What's your name?  ";
+	//getline(cin, name);
+	//HumanPlayer hp(name);
+	//SmartPlayer sp("Computer");
+	//Game g1(4, 3, 3, &hp, &sp);
+	//g1.play();
+	//cout << endl << "This time I'll go first." << endl;
+	//Game g2(4, 3, 3, &sp, &hp);
+	//g2.play();
 }
